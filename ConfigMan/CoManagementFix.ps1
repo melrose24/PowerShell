4586d259f1dbec2da47ae2f2c44260c2e6183584
Jason Melrose <melrose24@gmail.com>
2024-06-27 22:34:31 -0500

    Update CoManagementFix.ps1
    change seconds to 1

--- ConfigMan/CoManagementFix.ps1
+++ ConfigMan/CoManagementFix.ps1
@@ -11,7 +11,7 @@ Invoke-Command $ComputerName -ScriptBlock
         If ($Guid) 
         {
             Remove-Item -Path "HKLM:\Software\Microsoft\Enrollments\$guid" -Recurse
-            Start-Sleep -Seconds 5
+            Start-Sleep -Seconds 1
             Start-Process -FilePath 'c:\windows\CCMSETUP\ccmsetup.exe' -ArgumentList '/forceinstall cminstalldir=c:\windows\ccm ccmloglevel=0 cclogmaxsize=5242880 disablesiteopt=true DisableCache0pt=True smscachedir=c:\windows\ccmcache smscachesize=25 smscacheflags=PERCENTDISKSPACE SMSSITECODE=Auto' -Wait 
         }
     }
