0b8c80a03ee12e8fff37d74460907d4c9beec594
Jason Melrose <melrose24@gmail.com>
2024-06-27 21:05:18 -0500

    Update CoManagementFix.ps1
    updated install dir

--- ConfigMan/CoManagementFix.ps1
+++ ConfigMan/CoManagementFix.ps1
@@ -12,7 +12,7 @@ Invoke-Command  $ComputerName  -ScriptBlock
         {
             Remove-Item -Path "HKLM:\Software\Microsoft\Enrollments\$guid" -Recurse
             Start-Sleep -Seconds 5
-            Start-Process -FilePath 'c:\windows\CCMSETUP\ccmsetup.exe' -ArgumentList '/forceinstall cminstalldir=c:\windows\ccm ccmloglevel=0 cclogmaxsize=5242880 disablesiteopt=true DisableCache0pt=True smscachedir=c:\prod\WSD smscachesize=25 smscacheflags=PERCENTDISKSPACE SMSSITECODE=Auto' -Wait 
+            Start-Process -FilePath 'c:\windows\CCMSETUP\ccmsetup.exe' -ArgumentList '/forceinstall cminstalldir=c:\windows\ccm ccmloglevel=0 cclogmaxsize=5242880 disablesiteopt=true DisableCache0pt=True smscachedir=c:\windows\ccmcache smscachesize=25 smscacheflags=PERCENTDISKSPACE SMSSITECODE=Auto' -Wait 
         }
     }
 }
