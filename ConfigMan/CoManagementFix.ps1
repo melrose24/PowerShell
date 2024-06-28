e2eefb71632b801177e3acc7ab57bd1ea8c49e2e
Jason Melrose <melrose24@gmail.com>
2024-06-27 22:33:37 -0500

    Update CoManagementFix.ps1
    missing \ between software\microsoft

--- ConfigMan/CoManagementFix.ps1
+++ ConfigMan/CoManagementFix.ps1
@@ -1,14 +1,14 @@
 
-$devices = Get-Content -Path "C:\path\to\your\device_list.txt"
+$Devices = Get-Content -Path "C:\path\to\your\device_list.txt"
 
-foreach ($ComputerName in $devices) 
+foreach ($ComputerName in $Devices) 
 {
-Invoke-Command  $ComputerName  -ScriptBlock 
+Invoke-Command $ComputerName -ScriptBlock 
     {
-    $guid = Get-ChildItem HKLM:\Software Microsoft\Enrollments |  ForEach-Object {Get-ItemProperty $_.pspath} | Where-Object {$_.DiscoveryServiceFullURL} | Foreach-Object {$_.PSChildName}
-    write-host $_.name0 
-    Write-Host $_.$guid
-        If ($guid) 
+    $Guid = Get-ChildItem HKLM:\Software\Microsoft\Enrollments |  ForEach-Object {Get-ItemProperty $_.pspath} |             Where-Object {$_.DiscoveryServiceFullURL} | Foreach-Object {$_.PSChildName}
+    Write-Host $_.Name0 
+    Write-Host $_.$Guid
+        If ($Guid) 
         {
             Remove-Item -Path "HKLM:\Software\Microsoft\Enrollments\$guid" -Recurse
             Start-Sleep -Seconds 5
