f59e56e0bf1e7bcbdd60a548e5ce9ffe8e77429a
Jason Melrose <melrose24@gmail.com>
2024-06-29 14:29:05 -0500

    Update CoManagementFix.ps1

--- ConfigMan/CoManagementFix.ps1
+++ ConfigMan/CoManagementFix.ps1
@@ -1,18 +1,17 @@
-
-$Devices = Get-Content -Path "C:\path\to\your\device_list.txt"
+$Devices = Get-Content -Path "C:\pathtoyour\device_list.txt"
 
 foreach ($ComputerName in $Devices) 
 {
-Invoke-Command $ComputerName -ScriptBlock 
+Invoke-Command -ComputerName $ComputerName -ScriptBlock 
     {
-    $Guid = Get-ChildItem HKLM:\Software\Microsoft\Enrollments | ForEach-Object {Get-ItemProperty $_.pspath} |             Where-Object {$_.DiscoveryServiceFullURL} | Foreach-Object {$_.PSChildName}
-    Write-Host $_.Name0 
-    Write-Host $_.Guid
-        If ($Guid) 
-        {
-            Remove-Item -Path "HKLM:\Software\Microsoft\Enrollments\$guid" -Recurse
-            Start-Sleep -Seconds 1
-            Start-Process -FilePath 'c:\windows\CCMSETUP\ccmsetup.exe' -ArgumentList '/forceinstall cminstalldir=c:\windows\ccm ccmloglevel=0 cclogmaxsize=5242880 disablesiteopt=true DisableCache0pt=True smscachedir=c:\windows\ccmcache smscachesize=25 smscacheflags=PERCENTDISKSPACE SMSSITECODE=Auto' -Wait 
-        }
+        $Guid = Get-ChildItem HKLM:\Software\Microsoft\Enrollments |  ForEach-Object {Get-ItemProperty $_.pspath} | Where-Object {$_.DiscoveryServiceFullURL} | Foreach-Object {$_.PSChildName}
+        Write-Host $_.Name0 
+        Write-Host $Guid
+            If ($Guid) 
+            {
+                Remove-Item -Path "HKLM:\Software\Microsoft\Enrollments\$guid" -Recurse
+                Start-Sleep -Seconds 1
+                Start-Process -FilePath 'c:\windows\CCMSETUP\ccmsetup.exe' -ArgumentList '/forceinstall ccminstalldir=c:\windows\ccm ccmloglevel=0 ccmlogmaxsize=5242880 disablesiteopt=true DisableCache0pt=True smscachedir=c:\windows\ccmcache smscachesize=25 smscacheflags=PERCENTDISKSPACE SMSSITECODE=Auto' -Wait 
+            }
     }
 }
